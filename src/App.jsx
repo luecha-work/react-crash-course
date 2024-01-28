2a07873e9af1c6c4e365ee3f42d78c1c52bcb5e4
luecha.kan <luecha.kanm@gmail.com>
2024-01-28 20:38:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Post from "./components/Post";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Post />;
+}
+
+export default App;
