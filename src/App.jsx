d919cc609d28d8aca457e729f7dac3c70ad0c54c
luecha.kan <luecha.kanm@gmail.com>
2024-01-28 21:54:08 +0700

    Add Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import "./App.css";
 import Post from "./components/Post";
+
+const add = (a, b) => {
+  return a + b;
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Post />;
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="Check out the full course!" />
+      <Post author="Test" body={1} />
+      <Post />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,22 @@
+import PropTypes from "prop-types";
 import React from "react";
+import classis from "./Post.module.css";
 
 const names = ["Maximilian", "Manuel"];
 
-const Post = () => {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+const Post = ({ author, body }) => {
+  console.log(typeof body);
   return (
-    <div>
-      <p>{chosenName}</p>
-      <p>React.js is awesome!</p>
+    <div className={classis.post}>
+      <p>{author ?? "-"}</p>
+      <p>{body ?? "-"}</p>
     </div>
   );
+};
+
+Post.propTypes = {
+  author: PropTypes.string.isRequired,
+  body: PropTypes.any,
 };
 
 export default Post;
